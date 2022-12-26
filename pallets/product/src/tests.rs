0534eff5fb99634490cb585a7c78cf4716312aa3
vivek <viveksharmapoudel@gmail.com>
2022-12-26 15:45:03 +0545

    add test case

--- pallets/product/src/tests.rs
+++ pallets/product/src/tests.rs
@@ -1,23 +1,43 @@
-use crate::{mock::*, Error};
-use frame_support::{assert_noop, assert_ok};
+use core::{ str::Bytes};
 
-// #[test]
-// fn it_works_for_default_value() {
-// 	new_test_ext().execute_with(|| {
-// 		// Dispatch a signed extrinsic.
-// 		assert_ok!(TemplateModule::do_something(RuntimeOrigin::signed(1), 42));
-// 		// Read pallet storage and assert an expected result.
-// 		assert_eq!(TemplateModule::something(), Some(42));
-// 	});
-// }
+use crate::{mock::{*, self}, Error, types::{ProductName, self, Product}};
+use frame_support::{assert_noop, assert_ok, BoundedVec};
+use frame_system::Config;
+use sp_core::ConstU32;
 
-// #[test]
-// fn correct_error_for_none_value() {
-// 	new_test_ext().execute_with(|| {
-// 		// Ensure the expected error is thrown when no value is present.
-// 		assert_noop!(
-// 			TemplateModule::cause_error(RuntimeOrigin::signed(1)),
-// 			Error::<Test>::NoneValue
-// 		);
-// 	});
-// }
+
+#[test]
+fn it_works_for_add_authorized_user() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+
+        //Add one account_id using root_user
+		assert_ok!(ProductModule::add_authorized_user(RuntimeOrigin::root(), 1));
+        assert_eq!(ProductModule::get_authorized_user(1),true);
+
+        // add onother account_id with existing user
+		assert_ok!(ProductModule::add_authorized_user(RuntimeOrigin::signed(1), 2));
+        assert_eq!(ProductModule::get_authorized_user(2),true);
+        
+        //fail case
+        assert_eq!(ProductModule::get_authorized_user(3),false);
+	});
+}
+
+#[test]
+fn correct_error_for_none_value() {
+	new_test_ext().execute_with(  || {
+		
+        // Ensure the expected error is thrown when no value is present.
+		assert_ok!(ProductModule::add_authorized_user(RuntimeOrigin::root(), 1));
+
+        let mut vec = BoundedVec::try_from(Vec::from("other")).unwrap();
+        let p =  types::Product::<Test>::new(vec.clone(),4,1,);
+        assert_ok!(ProductModule::add_product(RuntimeOrigin::signed(1), vec, 4));
+        let savedP:Product<Test>=ProductModule::get_product_info(1).unwrap(); 
+        
+        
+        assert_eq!(savedP, p);
+        
+	});
+}
